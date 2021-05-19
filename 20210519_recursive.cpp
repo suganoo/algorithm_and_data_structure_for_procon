057af1ec6c81c71991aa3caa87445da20146ac65
suganoo <suganoo2002@gmail.com>
2021-05-19 19:36:51 +0900

    update

---
+++ 20210519_recursive.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static const int N = 100;
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
+
+int n, M[N][N];
+int color[N], d[N], f[N], tt;
+
+int main() {
+  int u, v, k, i, j;
+  cin >> n;
+  cout << n << endl;
+}
